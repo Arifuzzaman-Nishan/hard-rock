2a9135757f158c29cb3120c43087057659678e83
Nishan <arifuzzamannishan@gmail.com>
2021-02-09 21:36:32 +0600

    error handle done

--- app.js
+++ app.js
@@ -8,14 +8,31 @@ const getDataFromApi = (url) => {
     fetch(url)
         .then(res => res.json())
         .then(data => songInformationDisplay(data.data))
+        .catch(error => {
+            getId("lyrics-text").innerText = "sorry something wrong try again later";
+        })
+
 }
 
 
 const searchSong = () => {
     getId("search-button").addEventListener("click", () => {
+        getId("lyrics-text").innerText = "";
+
         const searchText = getId("input-text").value;
         const url = `https://api.lyrics.ovh/suggest/${searchText}`;
-        getDataFromApi(url);
+
+        if (searchText === '') {
+            getId("song-container").innerHTML = "";
+            getId("input-text").value = "";
+            getId("lyrics-text").innerText = "sorry please enter a song name";
+            getId("lyrics-text").style.color = "tomato";
+        }
+        else {
+            getId("input-text").value = "";
+            getDataFromApi(url);
+        }
+
     })
 }
 
@@ -37,14 +54,24 @@ const songInformationDisplay = (songs) => {
             <h3 class="lyrics-name">${song.title}</h3>
             <p class="author lead">Album by <span>${song.artist.name}</span></p>
             <audio controls>
-             <source src="${song.preview}">
+               <source src="${song.preview}">
            </audio>
         </div>
         <div class="col-md-3 text-md-right text-center">
-            <button class="btn btn-success">Get Lyrics</button>
+            <button onclick = "getLyrics('${song.artist.name}','${song.title}')" class="btn btn-success">Get Lyrics</button>
         </div>
         `;
         songDiv.innerHTML = songInfo;
         songContainer.appendChild(songDiv);
     });
+}
+
+const getLyrics = (artistName, title) => {
+    const url = `https://api.lyrics.ovh/v1/${artistName}/${title}`;
+    fetch(url)
+        .then(res => res.json())
+        .then(data => {
+            const lyricsText = getId("lyrics-text");
+            lyricsText.innerText = data.lyrics;
+        });
 }
